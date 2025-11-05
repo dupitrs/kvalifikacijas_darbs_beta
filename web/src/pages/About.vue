3b285510255238b894416abbf6a99123d18e2640
Raivis Reinfelds <117306753+dupitrs@users.noreply.github.com>
2025-11-05 23:47:25 +0000

    test

--- web/src/pages/About.vue
+++ web/src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="p-6">
     <h1 class="text-3xl font-bold text-green-600">Par projektu</h1>
-    <router-link to="/" class="text-indigo-600 underline">← Atpakaļ uz sākumu</router-link>
+    <router-link to="/" class="text-indigo-600 underline">← Atpakaļ uz sākumu un cipa gejs btw</router-link>
   </main>
 </template>
