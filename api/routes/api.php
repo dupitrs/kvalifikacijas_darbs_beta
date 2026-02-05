59c9e05fae87310cc2dcdf0953903e90cd5af45e
Raivis Reinfelds <rraivis2006@gmail.com>
2026-02-05 02:40:49 +0200

    azure bckend 13

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
+Route::get('/whoami', function () {
+    return response()->json([
+        'base_path' => base_path(),
+        'app_env' => app()->environment(),
+        'routes_file_exists_root' => file_exists(base_path('routes/api.php')),
+        'routes_file_exists_api' => file_exists(base_path('api/routes/api.php')),
+    ]);
+});
+
 Route::get('/ping', function () {
     return response()->json(['ok' => true]);
 });
